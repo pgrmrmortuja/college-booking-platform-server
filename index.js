598da5bad9bad5d2d5898856b240af4f2d5cf775
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-07-16 22:55:11 +0600

    get college details

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/property-id/:id', async (req, res) => {
+    app.get('/college/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await propertyCollection.findOne(query);
+      const result = await collegeCollection.findOne(query);
       res.send(result);
     })
 
